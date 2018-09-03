b51808e1f35dac6eaee076d6a86fa27ac7761fe9
vcoronado2014 <vcoronado.alarcon@gmail.com>
2018-09-02 23:35:50 -0300

    cambios en las tarjetas

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -20,43 +20,21 @@ export class CardsPage {
   rendiciones: any [];
   usuarios: any[];
   votaciones: any[];
+  muro: any[];
 
   constructor(public navCtrl: NavController, public user: User, public api: Api, public toastCtrl: ToastController,
     public translateService: TranslateService, public loading: LoadingController) {
 
       this.ionViewLoaded();
-    //this.obtenerInicio();
-/*
-    this.cardItems = [
-      {
-        user: {
-          avatar: 'assets/img/marty-avatar.png',
-          name: 'Marty McFly'
-        },
-        date: 'November 5, 1955',
-        image: 'assets/img/advance-card-bttf.png',
-        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
-      },
-      {
-        user: {
-          avatar: 'assets/img/sarah-avatar.png.jpeg',
-          name: 'Sarah Connor'
-        },
-        date: 'May 12, 1984',
-        image: 'assets/img/advance-card-tmntr.jpg',
-        content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
-      },
-      {
-        user: {
-          avatar: 'assets/img/ian-avatar.png',
-          name: 'Dr. Ian Malcolm'
-        },
-        date: 'June 28, 1990',
-        image: 'assets/img/advance-card-jp.jpg',
-        content: 'Your scientists were so preoccupied with whether or not they could, that they didn\'t stop to think if they should.'
-      }
-    ];
-*/
+  }
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.obtenerInicioRefresh(refresher);
+      //refresher.complete();
+    }, 2000);
   }
   ionViewLoaded() {
     let loader = this.loading.create({
@@ -65,38 +43,80 @@ export class CardsPage {
   
     loader.present().then(() => {
       this.obtenerInicio(loader);
-      /*
-      this.cardItems = [
-        {
-          user: {
-            avatar: 'assets/img/marty-avatar.png',
-            name: 'Marty McFly'
-          },
-          date: 'November 5, 1955',
-          image: 'assets/img/advance-card-bttf.png',
-          content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
-        },
-        {
-          user: {
-            avatar: 'assets/img/sarah-avatar.png.jpeg',
-            name: 'Sarah Connor'
-          },
-          date: 'May 12, 1984',
-          image: 'assets/img/advance-card-tmntr.jpg',
-          content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
-        },
-        {
-          user: {
-            avatar: 'assets/img/ian-avatar.png',
-            name: 'Dr. Ian Malcolm'
-          },
-          date: 'June 28, 1990',
-          image: 'assets/img/advance-card-jp.jpg',
-          content: 'Your scientists were so preoccupied with whether or not they could, that they didn\'t stop to think if they should.'
-        }
-      ];
-      */
     });
+  }
+  obtenerMuro(loader){
+    var instId = sessionStorage.getItem("INST_ID");
+    var usuId = sessionStorage.getItem("USU_ID");
+    let dataGet = { InstId: instId, UsuId: usuId };
+    let seq = this.api.post('Muro', dataGet).share();
+
+    seq.subscribe((res: any) => {
+      let retorno = res;
+      if (retorno != null){
+        this.muro = retorno;
+        var ultimo = this.muro.pop();
+        var card = {
+          user: {
+            avatar: '',
+            name: 'Novedades'
+          },
+          date: ultimo.FechaCreacion,//poner la ultima fecha
+          image: '',
+          content: 'Existen ' + (this.muro.length + 1) + ' novedades.',
+          icon: 'chatbubbles',
+          suma: this.muro.length + 1
+        };
+        this.cardItems.push(card);
+        loader.dismiss();
+      }
+      else {
+        loader.dismiss();
+      }
+
+    }, err => {
+      console.error('ERROR', err);
+      loader.dismiss();
+    });
+
+    return seq;
+  }
+
+  obtenerMuroRefresh(refresher){
+    var instId = sessionStorage.getItem("INST_ID");
+    var usuId = sessionStorage.getItem("USU_ID");
+    let dataGet = { InstId: instId, UsuId: usuId };
+    let seq = this.api.post('Muro', dataGet).share();
+
+    seq.subscribe((res: any) => {
+      let retorno = res;
+      if (retorno != null){
+        this.muro = retorno;
+        var ultimo = this.muro.pop();
+        var card = {
+          user: {
+            avatar: '',
+            name: 'Novedades'
+          },
+          date: ultimo.FechaCreacion,//poner la ultima fecha
+          image: '',
+          content: 'Existen ' + (this.muro.length + 1) + ' novedades.',
+          icon: 'chatbubbles',
+          suma: this.muro.length + 1
+        };
+        this.cardItems.push(card);
+        refresher.complete();
+      }
+      else {
+        refresher.complete();
+      }
+
+    }, err => {
+      console.error('ERROR', err);
+      refresher.complete();
+    });
+
+    return seq;
   }
 
   obtenerInicio(loader) {
@@ -123,9 +143,9 @@ export class CardsPage {
               },
               date: ultimo.NombreUsuario,//poner la ultima fecha
               image: '',
-              content: 'Existen ' + this.documentos.length + ' documentos creados.',
+              content: 'Existen ' + (this.documentos.length + 1) + ' documentos creados.',
               icon: 'document',
-              suma: this.documentos.length
+              suma: this.documentos.length + 1
             };
             this.cardItems.push(card);
 
@@ -142,17 +162,16 @@ export class CardsPage {
               },
               date: ultimo.NombreUsuario,//poner la ultima fecha
               image: '',
-              content: 'Existen ' + this.establecimientos.length + ' establecimientos creados.',
+              content: 'Existen ' + (this.establecimientos.length + 1) + ' establecimientos creados.',
               icon: 'home',
-              suma: this.establecimientos.length
+              suma: this.establecimientos.length + 1
             };
             this.cardItems.push(card);
 
           }
         }
         if (retorno.Eventos){
-          if (retorno.Eventos.proposals){
-            this.eventos = retorno.Eventos.proposals;
+            this.eventos = retorno.Eventos;
             var ultimo = this.eventos.pop();
             var card = {
               user: {
@@ -161,13 +180,11 @@ export class CardsPage {
               },
               date: ultimo.NombreUsuario,//poner la ultima fecha
               image: '',
-              content: 'Existen ' + this.eventos.length + ' establecimientos creados.',
-              icon: 'home',
-              suma: this.eventos.length
+              content: 'Existen ' + (this.eventos.length + 1) + ' eventos creados.',
+              icon: 'calendar',
+              suma: this.eventos.length + 1
             };
             this.cardItems.push(card);
-
-          }
         }
         if (retorno.Proyectos){
           if (retorno.Proyectos.proposals){
@@ -180,9 +197,9 @@ export class CardsPage {
               },
               date: ultimo.NombreUsuario,//poner la ultima fecha
               image: '',
-              content: 'Existen ' + this.proyectos.length + ' establecimientos creados.',
-              icon: 'home',
-              suma: this.proyectos.length
+              content: 'Existen ' + (this.proyectos.length + 1) + ' establecimientos creados.',
+              icon: 'albums',
+              suma: this.proyectos.length + 1
             };
             this.cardItems.push(card);
 
@@ -199,15 +216,32 @@ export class CardsPage {
               },
               date: ultimo.NombreUsuario,//poner la ultima fecha
               image: '',
-              content: 'Existen ' + this.rendiciones.length + ' Rendiciones creadas.',
-              icon: 'home',
-              suma: this.rendiciones.length
+              content: 'Existen ' + (this.rendiciones.length + 1) + ' Rendiciones creadas.',
+              icon: 'logo-usd',
+              suma: this.rendiciones.length + 1
             };
             this.cardItems.push(card);
 
           }
         }
-        loader.dismiss();
+        if (retorno.Usuarios){
+            this.usuarios = retorno.Usuarios;
+            var ultimo = this.usuarios.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Usuarios'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.usuarios.length + 1) + ' creados.',
+              icon: 'contact',
+              suma: this.usuarios.length + 1
+            };
+            this.cardItems.push(card);
+        }
+        this.obtenerMuro(loader);
+        //loader.dismiss();
       }
       else{
         console.error('ERROR al obtener listado');
@@ -217,6 +251,142 @@ export class CardsPage {
     }, err => {
       console.error('ERROR', err);
       loader.dismiss();
+    });
+
+    return seq;
+  }
+  obtenerInicioRefresh(refresher) {
+    var instId = sessionStorage.getItem("INST_ID");
+    var rolId = sessionStorage.getItem("ROL_ID");
+    let dataGet = { InstId: instId, RolId: rolId, UsuId: 0, Tipo: '1' };
+    let seq = this.api.post('Inicio', dataGet).share();
+    this.cardItems = [];
+
+    seq.subscribe((res: any) => {
+      // If the API returned a successful response, mark the user as logged in
+      let retorno = res;
+      if (retorno != null){
+        //this._loggedIn(retorno);
+        //ahora procesamos los datos
+        if (retorno.Documentos){
+          if (retorno.Documentos.proposals){
+            this.documentos = retorno.Documentos.proposals;
+            var ultimo = this.documentos.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Documentos'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.documentos.length + 1) + ' documentos creados.',
+              icon: 'document',
+              suma: this.documentos.length + 1
+            };
+            this.cardItems.push(card);
+
+          }
+        }
+        if (retorno.Establecimientos){
+          if (retorno.Establecimientos.proposals){
+            this.establecimientos = retorno.Establecimientos.proposals;
+            var ultimo = this.establecimientos.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Establecimientos'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.establecimientos.length + 1) + ' establecimientos creados.',
+              icon: 'home',
+              suma: this.establecimientos.length + 1
+            };
+            this.cardItems.push(card);
+
+          }
+        }
+        if (retorno.Eventos){
+            this.eventos = retorno.Eventos;
+            var ultimo = this.eventos.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Eventos'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.eventos.length + 1) + ' eventos creados.',
+              icon: 'calendar',
+              suma: this.eventos.length + 1
+            };
+            this.cardItems.push(card);
+        }
+        if (retorno.Proyectos){
+          if (retorno.Proyectos.proposals){
+            this.proyectos = retorno.Proyectos.proposals;
+            var ultimo = this.proyectos.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Proyectos'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.proyectos.length + 1) + ' establecimientos creados.',
+              icon: 'albums',
+              suma: this.proyectos.length + 1
+            };
+            this.cardItems.push(card);
+
+          }
+        }
+        if (retorno.Rendiciones){
+          if (retorno.Rendiciones.proposals){
+            this.rendiciones = retorno.Rendiciones.proposals;
+            var ultimo = this.rendiciones.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Rendiciones'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.rendiciones.length + 1) + ' Rendiciones creadas.',
+              icon: 'logo-usd',
+              suma: this.rendiciones.length + 1
+            };
+            this.cardItems.push(card);
+
+          }
+        }
+        if (retorno.Usuarios){
+            this.usuarios = retorno.Usuarios;
+            var ultimo = this.usuarios.pop();
+            var card = {
+              user: {
+                avatar: '',
+                name: 'Usuarios'
+              },
+              date: ultimo.NombreUsuario,//poner la ultima fecha
+              image: '',
+              content: 'Existen ' + (this.usuarios.length + 1) + ' creados.',
+              icon: 'contact',
+              suma: this.usuarios.length + 1
+            };
+            this.cardItems.push(card);
+        }
+        //refresher.complete();
+        this.obtenerMuroRefresh(refresher);
+      }
+      else{
+        console.error('ERROR al obtener listado');
+        let mensaje = this.mostrarMensaje("Error al obtener Inio", 3000);
+       refresher.complete();
+      }
+    }, err => {
+      console.error('ERROR', err);
+      refresher.complete();
     });
 
     return seq;
